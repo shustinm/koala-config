f1f383195e187bd38c09ef6345d0420660f48f2e
Michael Shustin <michael.shustin@aquasec.com>
2023-09-16 12:48:53 +0300

    Added absolute lines in insert mode

--- lua/config_lazy/autocmds/misc.lua
+++ lua/config_lazy/autocmds/misc.lua
@@ -1,12 +1,28 @@
 local api = vim.api
 
-local config_autocmds = api.nvim_create_augroup('config', { clear = true })
+local config_autocmds = api.nvim_create_augroup("config", { clear = true })
 
 -- Enable spell for .txt files
-vim.api.nvim_create_autocmd('FileType', {
+vim.api.nvim_create_autocmd("FileType", {
 	group = config_autocmds,
-	pattern = { 'text' },
+	pattern = { "text" },
 	callback = function()
 		vim.opt_local.spell = true
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+	group = config_autocmds,
+	pattern = { "*" },
+	callback = function()
+		vim.opt_local.relativenumber = false
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	group = config_autocmds,
+	pattern = { "*" },
+	callback = function()
+		vim.opt_local.relativenumber = true
 	end,
 })
